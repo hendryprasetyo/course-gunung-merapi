ed7e2a195460ce2fbe07ec7d058804e49ab19aa1
hendryprasetyo <hendryprras@gmail.com>
2022-10-14 14:31:01 +0700

    menambakah navbar dan modifed dari default bang dea

--- tweak.js
+++ tweak.js
@@ -3,11 +3,24 @@ const wrapper = document.getElementById("wrapper")
 const newBox = document.getElementById("newBox")
 const productBox1 = document.getElementById("productBox1")
 const productBox2 = document.getElementById("productBox2")
+const cta = document.getElementById('cta')
+
+const nav = document.querySelector('nav ul')
+const toggle = document.querySelector('toggle-menu')
+
+function back(){
+  window.location.reload()
+}
+
+function toggleMenu (){
+  nav.classList.toggle('slide')
+}
 
 function getDetail(nomor) {
   console.log('produk: ', nomor)
   newBox.style.display = "block"
   wrapper.style.display = "none"
+  cta.style.display = "none"
 
   if (nomor === 1) {
     productBox1.style.display = "flex"
